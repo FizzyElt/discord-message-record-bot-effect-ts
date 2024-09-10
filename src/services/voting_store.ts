ea73641033433069f8fb0e671d534bbb82b6246f
FizzyElt <fizzyelt.dev@outlook.com>
2024-09-10 22:34:21 +0800

    vote store layer

--- src/services/voting_store.ts
+++ src/services/voting_store.ts
@@ -1,4 +1,4 @@
-import { Context, Effect, MutableHashSet, Ref } from "effect";
+import { Context, Effect, MutableHashSet, Ref, Layer, pipe } from "effect";
 
 export type VotingStore = MutableHashSet.MutableHashSet<string>;
 
@@ -24,3 +24,37 @@ export const removeVoting = (userId: string) =>
   );
 
 export const createVotingStore = () => Ref.make(MutableHashSet.empty<string>());
+
+// layer
+class VotingService extends Context.Tag("VotingService")<
+  VotingService,
+  {
+    isUserVoting: (userId: string) => Effect.Effect<boolean>;
+    addNewVoting: (userId: string) => Effect.Effect<void>;
+    removeVoting: (userId: string) => Effect.Effect<void>;
+  }
+>() {}
+
+export const VotingServiceLive = Layer.effect(
+  VotingService,
+  Effect.gen(function* () {
+    const votingStoreRef = yield* Ref.make(MutableHashSet.empty<string>());
+
+    const getVotingStore = () => Ref.get(votingStoreRef);
+
+    const isUserVoting = (userId: string) =>
+      pipe(getVotingStore(), Effect.map(MutableHashSet.has(userId)));
+
+    const addNewVoting = (userId: string) =>
+      Ref.update(votingStoreRef, MutableHashSet.add(userId));
+
+    const removeVoting = (userId: string) =>
+      Ref.update(votingStoreRef, MutableHashSet.remove(userId));
+
+    return {
+      isUserVoting,
+      addNewVoting,
+      removeVoting,
+    };
+  }),
+);
