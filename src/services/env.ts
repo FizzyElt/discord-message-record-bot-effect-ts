22178c1717a591f399989e3829114ac57f7fbbf0
FizzyElt <fizzyelt.dev@outlook.com>
2024-11-14 23:34:15 +0800

    migrate to bun

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,38 +1,36 @@
-import dotenv from "dotenv";
+import type { Env } from "bun";
 import { Context, Layer } from "effect";
 
-dotenv.config();
-
-// layer
-
-export interface Env {
-  readonly TOKEN: string;
-  readonly BOT_SENDING_CHANNEL_ID: string;
-  readonly BOT_SENDING_CHANNEL_NAME: string;
-  readonly LOG_CHANNEL_ID: string;
-  readonly ADMIN_ROLE_ID: string;
-  readonly CLIENT_ID: string;
-  readonly GUILD_ID: string;
-  readonly VOTE_ROLE_ID: string;
-  readonly TIMEZONE: string;
-  readonly CAT_API_KEY: string;
-  readonly EMOJI_KITCHEN_KEY: string;
-  readonly SUPABASE_URL_ADMIN: string;
+declare module "bun" {
+  interface Env {
+    readonly TOKEN: string;
+    readonly BOT_SENDING_CHANNEL_ID: string;
+    readonly BOT_SENDING_CHANNEL_NAME: string;
+    readonly LOG_CHANNEL_ID: string;
+    readonly ADMIN_ROLE_ID: string;
+    readonly CLIENT_ID: string;
+    readonly GUILD_ID: string;
+    readonly VOTE_ROLE_ID: string;
+    readonly TIMEZONE: string;
+    readonly CAT_API_KEY: string;
+    readonly EMOJI_KITCHEN_KEY: string;
+    readonly SUPABASE_URL_ADMIN: string;
+  }
 }
 
 export class EnvConfig extends Context.Tag("EnvConfig")<EnvConfig, Env>() {}
 
 export const EnvLive = Layer.succeed(EnvConfig, {
-  TOKEN: process.env.TOKEN || "",
-  BOT_SENDING_CHANNEL_ID: process.env.BOT_SENDING_CHANNEL_ID || "",
-  BOT_SENDING_CHANNEL_NAME: process.env.BOT_SENDING_CHANNEL_NAME || "",
-  LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID || "",
-  ADMIN_ROLE_ID: process.env.ADMIN_ROLE_ID || "",
-  CLIENT_ID: process.env.CLIENT_ID || "",
-  GUILD_ID: process.env.GUILD_ID || "",
-  VOTE_ROLE_ID: process.env.VOTE_ROLE_ID || "",
-  TIMEZONE: process.env.TIMEZONE || "",
-  CAT_API_KEY: process.env.CAT_API_KEY || "",
-  EMOJI_KITCHEN_KEY: process.env.EMOJI_KITCHEN_KEY || "",
-  SUPABASE_URL_ADMIN: process.env.SUPABASE_URL_ADMIN || "",
+  TOKEN: Bun.env.TOKEN,
+  BOT_SENDING_CHANNEL_ID: Bun.env.BOT_SENDING_CHANNEL_ID,
+  BOT_SENDING_CHANNEL_NAME: Bun.env.BOT_SENDING_CHANNEL_NAME,
+  LOG_CHANNEL_ID: Bun.env.LOG_CHANNEL_ID,
+  ADMIN_ROLE_ID: Bun.env.ADMIN_ROLE_ID,
+  CLIENT_ID: Bun.env.CLIENT_ID,
+  GUILD_ID: Bun.env.GUILD_ID,
+  VOTE_ROLE_ID: Bun.env.VOTE_ROLE_ID,
+  TIMEZONE: Bun.env.TIMEZONE,
+  CAT_API_KEY: Bun.env.CAT_API_KEY,
+  EMOJI_KITCHEN_KEY: Bun.env.EMOJI_KITCHEN_KEY,
+  SUPABASE_URL_ADMIN: Bun.env.SUPABASE_URL_ADMIN,
 });
