5cc839bb7b86ab763606732d30a410d4ea7503d0
FizzyElt <fizzyelt8786@gmail.com>
2026-03-15 00:22:18 +0800

    fix timezone format

--- src/tasks/other/get_off_work.ts
+++ src/tasks/other/get_off_work.ts
@@ -1,34 +1,29 @@
-import { format } from "date-fns";
+import { formatInTimeZone } from "date-fns-tz";
 import type {
     ChatInputCommandInteraction,
     CacheType,
     InteractionResponse,
 } from "discord.js";
 import { Effect, DateTime } from "effect";
-import { NoSuchElementException, UnknownException } from "effect/Cause";
+import { UnknownException } from "effect/Cause";
 
 import { bold } from "~/utils/mark_string";
 
 export const getOffWork = (
     interaction: ChatInputCommandInteraction<CacheType>,
-): Effect.Effect<
-    InteractionResponse<boolean>,
-    NoSuchElementException | UnknownException,
-    never
-> =>
+): Effect.Effect<InteractionResponse<boolean>, UnknownException, never> =>
     Effect.gen(function* () {
         const user = interaction.user;
 
         const now = yield* DateTime.now;
 
-        const zonedDate = yield* Effect.void.pipe(
-            Effect.flatMap(() =>
-                DateTime.makeZoned(now, { timeZone: "Asia/Taipei" }),
-            ),
-            Effect.map(DateTime.toDateUtc),
-        );
+        const date = now.pipe(DateTime.toDate);
 
-        const zonedDateString = format(zonedDate, "yyyy-MM-dd HH:mm:ssXXXX");
+        const zonedDateString = formatInTimeZone(
+            date,
+            "Asia/Taipei",
+            "yyyy-MM-dd HH:mm:ssXXXX",
+        );
 
         return yield* Effect.tryPromise(() =>
             interaction.reply({
